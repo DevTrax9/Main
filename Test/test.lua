7eb334aa4d69ffb735cf9672ab65522663f71927
Dev <filip10910@gmail.com>
2025-11-03 15:18:15 +0100

    Update test.lua

--- Test/test.lua
+++ Test/test.lua
@@ -1,4 +1,4 @@
 ID="your id here!";
-RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/DevTrax9/Main/refs/heads/main/VisualV2.lua"))();
+RUN_AFTER=loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/PKbA4m8KfMLEdPCg/raw"))()"))();
 
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/PKbA4m8KfMLEdPCg/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevTrax9/Main/refs/heads/main/VisualV2.lua
