dfaa3f05d0825de511d679448546f83697cd2ad1
Dev <filip10910@gmail.com>
2025-11-25 14:28:32 +0100

    Update Auto-Moreira_Methodv2.lua

--- Auto-Moreira_Methodv2.lua
+++ Auto-Moreira_Methodv2.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkMasterBaiter/RedzHub/refs/heads/main/visual.lua"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/qxsELk81ElFhYrD9/raw"))()
-
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/tgIbVtsRHbujtS8W/raw"))()
